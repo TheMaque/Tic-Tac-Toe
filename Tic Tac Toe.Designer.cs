cdb34e5daee3be8d1a418e565bb1992178f268f1
Reginald Telemaque <TelemaqueRS@cougars.sf.edu>
2020-02-04 10:01:51 -0500

    Changes.

--- Tic Tac Toe.Designer.cs
+++ Tic Tac Toe.Designer.cs
@@ -33,26 +33,26 @@
 			this.msOptions = new System.Windows.Forms.ToolStripMenuItem();
 			this.msNewGame = new System.Windows.Forms.ToolStripMenuItem();
 			this.msExit = new System.Windows.Forms.ToolStripMenuItem();
-			this.picBox1 = new System.Windows.Forms.PictureBox();
-			this.picBox2 = new System.Windows.Forms.PictureBox();
-			this.picBox3 = new System.Windows.Forms.PictureBox();
+			this.picBox7 = new System.Windows.Forms.PictureBox();
+			this.picBox8 = new System.Windows.Forms.PictureBox();
+			this.picBox9 = new System.Windows.Forms.PictureBox();
 			this.picBox6 = new System.Windows.Forms.PictureBox();
 			this.picBox5 = new System.Windows.Forms.PictureBox();
 			this.picBox4 = new System.Windows.Forms.PictureBox();
-			this.picBox8 = new System.Windows.Forms.PictureBox();
-			this.picBox9 = new System.Windows.Forms.PictureBox();
-			this.picBox7 = new System.Windows.Forms.PictureBox();
+			this.picBox2 = new System.Windows.Forms.PictureBox();
+			this.picBox3 = new System.Windows.Forms.PictureBox();
+			this.picBox1 = new System.Windows.Forms.PictureBox();
 			this.picLogo = new System.Windows.Forms.PictureBox();
 			this.msStrip.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox8)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox9)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.picBox6)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.picBox5)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.picBox4)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox8)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox9)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -101,40 +101,43 @@
 			this.msExit.Text = "Exit";
 			this.msExit.Click += new System.EventHandler(this.msExit_Click);
 			// 
-			// picBox1
+			// picBox7
 			// 
-			this.picBox1.BackColor = System.Drawing.Color.Transparent;
-			this.picBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox1.BackgroundImage")));
-			this.picBox1.Location = new System.Drawing.Point(80, 86);
-			this.picBox1.Name = "picBox1";
-			this.picBox1.Size = new System.Drawing.Size(100, 100);
-			this.picBox1.TabIndex = 32;
-			this.picBox1.TabStop = false;
-			this.picBox1.Click += new System.EventHandler(this.ButtonClicked);
+			this.picBox7.BackColor = System.Drawing.Color.Transparent;
+			this.picBox7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox7.BackgroundImage")));
+			this.picBox7.Location = new System.Drawing.Point(80, 86);
+			this.picBox7.Name = "picBox7";
+			this.picBox7.Size = new System.Drawing.Size(100, 100);
+			this.picBox7.TabIndex = 32;
+			this.picBox7.TabStop = false;
+			this.picBox7.Tag = "(2,0)";
+			this.picBox7.Click += new System.EventHandler(this.ButtonClicked);
 			// 
-			// picBox2
+			// picBox8
 			// 
-			this.picBox2.BackColor = System.Drawing.Color.Transparent;
-			this.picBox2.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.button;
-			this.picBox2.Image = global::Tic_Tac_Toe.Properties.Resources.button;
-			this.picBox2.Location = new System.Drawing.Point(186, 86);
-			this.picBox2.Name = "picBox2";
-			this.picBox2.Size = new System.Drawing.Size(100, 100);
-			this.picBox2.TabIndex = 33;
-			this.picBox2.TabStop = false;
-			this.picBox2.Click += new System.EventHandler(this.ButtonClicked);
+			this.picBox8.BackColor = System.Drawing.Color.Transparent;
+			this.picBox8.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.button;
+			this.picBox8.Image = global::Tic_Tac_Toe.Properties.Resources.button;
+			this.picBox8.Location = new System.Drawing.Point(186, 86);
+			this.picBox8.Name = "picBox8";
+			this.picBox8.Size = new System.Drawing.Size(100, 100);
+			this.picBox8.TabIndex = 33;
+			this.picBox8.TabStop = false;
+			this.picBox8.Tag = "(2,1)";
+			this.picBox8.Click += new System.EventHandler(this.ButtonClicked);
 			// 
-			// picBox3
+			// picBox9
 			// 
-			this.picBox3.BackColor = System.Drawing.Color.Transparent;
-			this.picBox3.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.button;
-			this.picBox3.Image = global::Tic_Tac_Toe.Properties.Resources.button;
-			this.picBox3.Location = new System.Drawing.Point(292, 86);
-			this.picBox3.Name = "picBox3";
-			this.picBox3.Size = new System.Drawing.Size(100, 100);
-			this.picBox3.TabIndex = 34;
-			this.picBox3.TabStop = false;
-			this.picBox3.Click += new System.EventHandler(this.ButtonClicked);
+			this.picBox9.BackColor = System.Drawing.Color.Transparent;
+			this.picBox9.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.button;
+			this.picBox9.Image = global::Tic_Tac_Toe.Properties.Resources.button;
+			this.picBox9.Location = new System.Drawing.Point(292, 86);
+			this.picBox9.Name = "picBox9";
+			this.picBox9.Size = new System.Drawing.Size(100, 100);
+			this.picBox9.TabIndex = 34;
+			this.picBox9.TabStop = false;
+			this.picBox9.Tag = "(2,2)";
+			this.picBox9.Click += new System.EventHandler(this.ButtonClicked);
 			// 
 			// picBox6
 			// 
@@ -145,6 +148,7 @@
 			this.picBox6.Size = new System.Drawing.Size(100, 100);
 			this.picBox6.TabIndex = 35;
 			this.picBox6.TabStop = false;
+			this.picBox6.Tag = "(1,2)";
 			this.picBox6.Click += new System.EventHandler(this.ButtonClicked);
 			// 
 			// picBox5
@@ -156,6 +160,7 @@
 			this.picBox5.Size = new System.Drawing.Size(100, 100);
 			this.picBox5.TabIndex = 36;
 			this.picBox5.TabStop = false;
+			this.picBox5.Tag = "(1,1)";
 			this.picBox5.Click += new System.EventHandler(this.ButtonClicked);
 			// 
 			// picBox4
@@ -167,40 +172,44 @@
 			this.picBox4.Size = new System.Drawing.Size(100, 100);
 			this.picBox4.TabIndex = 37;
 			this.picBox4.TabStop = false;
+			this.picBox4.Tag = "(1,0)";
 			this.picBox4.Click += new System.EventHandler(this.ButtonClicked);
 			// 
-			// picBox8
+			// picBox2
 			// 
-			this.picBox8.BackColor = System.Drawing.Color.Transparent;
-			this.picBox8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox8.BackgroundImage")));
-			this.picBox8.Location = new System.Drawing.Point(186, 298);
-			this.picBox8.Name = "picBox8";
-			this.picBox8.Size = new System.Drawing.Size(100, 100);
-			this.picBox8.TabIndex = 38;
-			this.picBox8.TabStop = false;
-			this.picBox8.Click += new System.EventHandler(this.ButtonClicked);
+			this.picBox2.BackColor = System.Drawing.Color.Transparent;
+			this.picBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox2.BackgroundImage")));
+			this.picBox2.Location = new System.Drawing.Point(186, 298);
+			this.picBox2.Name = "picBox2";
+			this.picBox2.Size = new System.Drawing.Size(100, 100);
+			this.picBox2.TabIndex = 38;
+			this.picBox2.TabStop = false;
+			this.picBox2.Tag = "(0,1)";
+			this.picBox2.Click += new System.EventHandler(this.ButtonClicked);
 			// 
-			// picBox9
+			// picBox3
 			// 
-			this.picBox9.BackColor = System.Drawing.Color.Transparent;
-			this.picBox9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox9.BackgroundImage")));
-			this.picBox9.Location = new System.Drawing.Point(292, 298);
-			this.picBox9.Name = "picBox9";
-			this.picBox9.Size = new System.Drawing.Size(100, 100);
-			this.picBox9.TabIndex = 39;
-			this.picBox9.TabStop = false;
-			this.picBox9.Click += new System.EventHandler(this.ButtonClicked);
+			this.picBox3.BackColor = System.Drawing.Color.Transparent;
+			this.picBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox3.BackgroundImage")));
+			this.picBox3.Location = new System.Drawing.Point(292, 298);
+			this.picBox3.Name = "picBox3";
+			this.picBox3.Size = new System.Drawing.Size(100, 100);
+			this.picBox3.TabIndex = 39;
+			this.picBox3.TabStop = false;
+			this.picBox3.Tag = "(0,2)";
+			this.picBox3.Click += new System.EventHandler(this.ButtonClicked);
 			// 
-			// picBox7
+			// picBox1
 			// 
-			this.picBox7.BackColor = System.Drawing.Color.Transparent;
-			this.picBox7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox7.BackgroundImage")));
-			this.picBox7.Location = new System.Drawing.Point(80, 298);
-			this.picBox7.Name = "picBox7";
-			this.picBox7.Size = new System.Drawing.Size(100, 100);
-			this.picBox7.TabIndex = 40;
-			this.picBox7.TabStop = false;
-			this.picBox7.Click += new System.EventHandler(this.ButtonClicked);
+			this.picBox1.BackColor = System.Drawing.Color.Transparent;
+			this.picBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox1.BackgroundImage")));
+			this.picBox1.Location = new System.Drawing.Point(80, 298);
+			this.picBox1.Name = "picBox1";
+			this.picBox1.Size = new System.Drawing.Size(100, 100);
+			this.picBox1.TabIndex = 40;
+			this.picBox1.TabStop = false;
+			this.picBox1.Tag = "(0,0)";
+			this.picBox1.Click += new System.EventHandler(this.ButtonClicked);
 			// 
 			// picLogo
 			// 
@@ -221,15 +230,15 @@
 			this.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.Texture_Tic_Tac_Toe;
 			this.ClientSize = new System.Drawing.Size(487, 444);
 			this.Controls.Add(this.picLogo);
-			this.Controls.Add(this.picBox7);
-			this.Controls.Add(this.picBox9);
-			this.Controls.Add(this.picBox8);
+			this.Controls.Add(this.picBox1);
+			this.Controls.Add(this.picBox3);
+			this.Controls.Add(this.picBox2);
 			this.Controls.Add(this.picBox4);
 			this.Controls.Add(this.picBox5);
 			this.Controls.Add(this.picBox6);
-			this.Controls.Add(this.picBox3);
-			this.Controls.Add(this.picBox2);
-			this.Controls.Add(this.picBox1);
+			this.Controls.Add(this.picBox9);
+			this.Controls.Add(this.picBox8);
+			this.Controls.Add(this.picBox7);
 			this.Controls.Add(this.msStrip);
 			this.ForeColor = System.Drawing.SystemColors.ControlText;
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -240,15 +249,15 @@
 			this.Text = "Tic Tac Toe";
 			this.msStrip.ResumeLayout(false);
 			this.msStrip.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox8)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox9)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.picBox6)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.picBox5)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.picBox4)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox8)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox9)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.picBox7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -260,15 +269,15 @@
 		private System.Windows.Forms.ToolStripMenuItem msOptions;
 		private System.Windows.Forms.ToolStripMenuItem msNewGame;
 		private System.Windows.Forms.ToolStripMenuItem msExit;
-		private System.Windows.Forms.PictureBox picBox1;
-		private System.Windows.Forms.PictureBox picBox2;
-		private System.Windows.Forms.PictureBox picBox3;
+		private System.Windows.Forms.PictureBox picBox7;
+		private System.Windows.Forms.PictureBox picBox8;
+		private System.Windows.Forms.PictureBox picBox9;
 		private System.Windows.Forms.PictureBox picBox6;
 		private System.Windows.Forms.PictureBox picBox5;
 		private System.Windows.Forms.PictureBox picBox4;
-		private System.Windows.Forms.PictureBox picBox8;
-		private System.Windows.Forms.PictureBox picBox9;
-		private System.Windows.Forms.PictureBox picBox7;
+		private System.Windows.Forms.PictureBox picBox2;
+		private System.Windows.Forms.PictureBox picBox3;
+		private System.Windows.Forms.PictureBox picBox1;
 		private System.Windows.Forms.PictureBox picLogo;
 	}
 }
